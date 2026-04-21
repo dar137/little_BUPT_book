a99a5556f772f7e340c7f1bcd3077ddd3e37ef59
hzy <2861766003@qq.com>
2026-04-21 12:17:27 +0800

    修复

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>📚 小邮书 BBS v0.1</h1>
+      <p>前端开发中... 2号和3号的第一个协作项目！</p>
+      <p style={{ color: 'blue' }}>今天是 2026 年 4 月 21 日，我们重新出发！</p>
+    </div>
+  )
+}
+export default App
